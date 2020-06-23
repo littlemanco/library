16bf97f2976249105ff85eb7d26ed7305f175ef5
Andrew Howden <hello@andrewhowden.com>
2020-06-23 16:05:18 +0200

    Initial implementation of ebook reader

---
+++ src/internal/book/httpbook.go
@@ -0,0 +1,70 @@
+package book
+
+import (
+	"bytes"
+	"net/http"
+
+	"github.com/kapmahc/epub"
+	"github.com/pkg/errors"
+)
+
+// HTTPBook represents a "servable book"
+type HTTPBook struct {
+
+	// Book is the actual book being served
+	Book *epub.Book
+}
+
+// Handler is the HTTP handler that serves the appropriate book content
+func (h HTTPBook) Handler(w http.ResponseWriter, r *http.Request) {
+	path := r.RequestURI
+
+	// In the case this is the root, transform the root into the nav file.
+	if path == "/" {
+		path = "/nav.xhtml"
+	}
+
+	file, err := h.Book.Open(path)
+	if err != nil {
+		// Todo: Logging Here
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+	}
+
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(file)
+	w.Write(buf.Bytes())
+}
+
+// New creates a new HTTPBook entity
+func New(options ...func(*HTTPBook) error) (*HTTPBook, error) {
+	b := &HTTPBook{}
+
+	for _, o := range options {
+		if err := o(b); err != nil {
+			return nil, errors.Wrap(err, "unable to persist option to Book")
+		}
+	}
+
+	// Check required properties
+	if b.Book == nil {
+		return nil, errors.New("cannot create http book: no book supplied")
+	}
+
+	return b, nil
+}
+
+// WithBook adds the book to the HTTPBook
+func WithBook(path string) func(*HTTPBook) error {
+	return func(h *HTTPBook) error {
+		book, err := epub.Open(path)
+
+		if err != nil {
+			return errors.Wrap(err, "unable to open book")
+		}
+
+		h.Book = book
+
+		return nil
+	}
+}
--- src/internal/server/server.go
+++ src/internal/server/server.go
@@ -3,6 +3,8 @@ package server
 import (
 	"net/http"
 
+	"github.com/pkg/errors"
+	"go.pkg.littleman.co/library/internal/book"
 	"go.pkg.littleman.co/library/internal/server/handlers"
 )
 
@@ -20,7 +22,14 @@ func New(options ...func(*Server) error) (*Server, error) {
 
 // Serve starts the server
 func (s Server) Serve() error {
+	httpBook, err := book.New(book.WithBook("/usr/share/library/incident-management.epub"))
+
+	if err != nil {
+		return errors.Wrap(err, "unable to create http book")
+	}
+
 	http.HandleFunc("/healthz", handlers.NoContent)
+	http.HandleFunc("/", httpBook.Handler)
 
 	return http.ListenAndServe(s.address, nil)
 }
