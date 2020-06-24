54bd799e385f19dc7d92929e0db8796360ed4f0e
Andrew Howden <hello@andrewhowden.com>
2020-06-24 19:14:36 +0200

    Allow reading configuration from environment

--- src/cmd/root.go
+++ src/cmd/root.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/dedelala/sysexits"
 	"github.com/spf13/cobra"
@@ -48,10 +49,12 @@ func initConfig() {
 		viper.SetConfigName(".library")
 	}
 
-	viper.AutomaticEnv() // read in environment variables that match
-
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
 		fmt.Println("Using config file:", viper.ConfigFileUsed())
 	}
+
+	viper.SetEnvPrefix("LIBRARY_")
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "__"))
+	viper.AutomaticEnv()
 }
